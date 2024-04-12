acdf29728f38e9c7ad40a2e52868b08fbd6c57d3
Vinícius Souza <dev.souzavinicius@gamil.com>
2024-04-12 15:57:52 -0300

    feat: text translation applied

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,37 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../styles";
 
+import "../../utils/languages/i18n";
+
+import { useTranslation } from "react-i18next";
+
 export function Home() {
+  const { t, i18n } = useTranslation();
+
+  function changeLanguage(value: string) {
+    i18n.changeLanguage(value);
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text>Home component works</Text>
+        <View
+          style={{
+            width: "100%",
+            alignContent: "center",
+            gap: 10,
+            justifyContent: "center",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity onPress={() => changeLanguage("en")}>
+            <Text>English</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text onPress={() => changeLanguage("pt")}>Português</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={{ marginTop: 20 }}>{t("traslationText")}</Text>
       </View>
     </SafeAreaView>
   );
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'red',
+      backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
